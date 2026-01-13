33b7a3152fb17d54cc4d50a5b6ae07300c01e065
igorkll <frerther@gmail.com>
2026-01-13 20:31:26 +0300

    backup

---
+++ README.md
@@ -0,0 +1,5 @@
+# efivars-cli-powershell
+* CLI for efivars for windows
+
+## usage:
+* powershell -ExecutionPolicy Bypass -File "efivars_auto_write.ps1"
---
+++ efivars_auto_write.ps1
@@ -0,0 +1,104 @@
+# ------------ EFI vars writing api unlock
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+[StructLayout(LayoutKind.Sequential)]
+public struct LUID
+{
+    public uint LowPart;
+    public int HighPart;
+}
+
+[StructLayout(LayoutKind.Sequential)]
+public struct TOKEN_PRIVILEGES
+{
+    public uint PrivilegeCount;
+    public LUID Luid;
+    public uint Attributes;
+}
+
+public class AdjPriv
+{
+    [DllImport("advapi32.dll", SetLastError = true)]
+    public static extern bool OpenProcessToken(IntPtr ProcessHandle, uint DesiredAccess, out IntPtr TokenHandle);
+
+    [DllImport("kernel32.dll")]
+    public static extern IntPtr GetCurrentProcess();
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    public static extern bool LookupPrivilegeValue(string lpSystemName, string lpName, out LUID lpLuid);
+
+    [DllImport("advapi32.dll", SetLastError = true)]
+    public static extern bool AdjustTokenPrivileges(IntPtr TokenHandle, bool DisableAllPrivileges,
+        ref TOKEN_PRIVILEGES NewState, int BufferLength, IntPtr PreviousState, IntPtr ReturnLength);
+
+    public const uint TOKEN_ADJUST_PRIVILEGES = 0x20;
+    public const uint TOKEN_QUERY = 0x8;
+    public const uint SE_PRIVILEGE_ENABLED = 0x2;
+
+    public static bool EnablePrivilege(string privilege)
+    {
+        IntPtr token;
+        if (!OpenProcessToken(GetCurrentProcess(), TOKEN_ADJUST_PRIVILEGES | TOKEN_QUERY, out token))
+            return false;
+
+        LUID luid;
+        if (!LookupPrivilegeValue(null, privilege, out luid))
+            return false;
+
+        TOKEN_PRIVILEGES tp = new TOKEN_PRIVILEGES();
+        tp.PrivilegeCount = 1;
+        tp.Luid = luid;
+        tp.Attributes = SE_PRIVILEGE_ENABLED;
+
+        return AdjustTokenPrivileges(token, false, ref tp, 0, IntPtr.Zero, IntPtr.Zero);
+    }
+}
+"@
+if (-not [AdjPriv]::EnablePrivilege("SeSystemEnvironmentPrivilege")) {
+    Write-Host "The SeSystemEnvironmentPrivilege privilege could not be enabled"
+    exit
+}
+
+# ------------ EFI vars writing
+
+Add-Type @"
+using System;
+using System.Runtime.InteropServices;
+
+public class EFI {
+    [DllImport("kernel32.dll", SetLastError=true, CharSet=CharSet.Unicode)]
+    public static extern bool SetFirmwareEnvironmentVariable(
+        string lpName,
+        string lpGuid,
+        byte[] pValue,
+        uint nSize
+    );
+}
+"@
+
+function Set-EFIVar {
+    param(
+        [string]$Name,
+        [string]$Guid,
+        [byte[]]$Value
+    )
+
+    Write "Attempt to write an EFI-variable: [$Guid] ($Name) = $Value"
+    $result = [EFI]::SetFirmwareEnvironmentVariable($Name, $Guid, $Value, $Value.Length)
+
+    if ($result) {
+        return $true
+    } else {
+        $err = [System.Runtime.InteropServices.Marshal]::GetLastWin32Error()
+        Write-Warning "Failed to set EFI-variable: $err"
+        return $false
+    }
+}
+
+$name = "Timeout"
+$guid = "{8BE4DF61-93CA-11D2-AA0D-00E098032B8C}"
+$value = [byte[]](0x00, 0x00)
+
+Set-EFIVar -Name $name -Guid $guid -Value $value
